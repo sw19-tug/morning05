ce24fca7728949bf14ddcddf23a84e09a134cfc8
Hanna Mueller <h.mueller@student.tugraz.at>
2019-03-20 10:00:56 +0100

    GAMES_001 add test for game buttons [LF, MH]

--- app/src/androidTest/java/com/example/sw19_morning05/MainActivityEspressoTest.java
+++ app/src/androidTest/java/com/example/sw19_morning05/MainActivityEspressoTest.java
@@ -7,7 +7,7 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,12 +15,11 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class MainActivityEspressoTest {
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.example.sw19_morning05", appContext.getPackageName());
+    public void testGameButtonsVisible() {
+        onView(withId(R.id.bt_tictactoe)).check(matches(isDisplayed()));
+        onView(withId(R.id.bt_hangman)).check(matches(isDisplayed()));
+        onView(withId(R.id.bt_touchtheblock)).check(matches(isDisplayed()));
     }
 }
