3aed53ce236262509608e6c97e8034e2e494476f
Frederic Linn <frederic.linn@student.tugraz.at>
2019-03-20 10:51:13 +0100

    GAMES_001 added three buttons [MH, LF]

--- app/src/androidTest/java/com/example/sw19_morning05/MainActivityEspressoTest.java
+++ app/src/androidTest/java/com/example/sw19_morning05/MainActivityEspressoTest.java
@@ -3,10 +3,16 @@ package com.example.sw19_morning05;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.Rule;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -16,6 +22,8 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityEspressoTest {
+    @Rule
+public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
     @Test
     public void testGameButtonsVisible() {
         onView(withId(R.id.bt_tictactoe)).check(matches(isDisplayed()));
