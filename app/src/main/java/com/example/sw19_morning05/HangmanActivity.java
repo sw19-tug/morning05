63f66177d8c53c0dfae2f6de123094b204d04a3f
stefanie0507 <stefanie.plasch@student.tugraz.at>
2019-03-20 11:02:53 +0100

    GAMES_006 created Hangman Activity [BL, PS]

---
+++ app/src/main/java/com/example/sw19_morning05/HangmanActivity.java
@@ -0,0 +1,13 @@
+package com.example.sw19_morning05;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HangmanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hangman);
+    }
+}
