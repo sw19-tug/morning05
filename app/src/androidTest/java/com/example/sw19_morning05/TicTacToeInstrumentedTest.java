9a2e60973c1a4a44a3efe4feb826f841e22d1162
Julia <julia.noestl@student.tugraz.at>
2019-03-20 14:31:18 +0100

    GAMES_002 add tests for button of field [DJ,JN]

--- app/src/androidTest/java/com/example/sw19_morning05/TicTacToeInstrumentedTest.java
+++ app/src/androidTest/java/com/example/sw19_morning05/TicTacToeInstrumentedTest.java
@@ -8,11 +8,14 @@ import android.support.test.rule.ActivityTestRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.app.PendingIntent.getActivity;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.v4.graphics.drawable.IconCompat.getResources;
 
 
 /**
@@ -29,6 +32,22 @@ public class TicTacToeInstrumentedTest {
     public void testTextViewVisible() {
         onView(withId(R.id.tv_header)).check(matches(isDisplayed()));
         onView(withId(R.id.tv_header)).check(matches(withText("Tic Tac Toe")));
+    }
+    @Test
+    public void testFieldbuttonClickable(){
+        onView(withId(R.id.bt_field00)).check(matches(isDisplayed()));
+        onView(withId(R.id.bt_field00)).perform(click());
 
     }
+    @Test void testNoTextInFields(){
+        onView(withId(R.id.bt_field00)).check(matches(withText("")));
+    }
+    /*@Test void testBoardOnStart(){
+        for(int i = 0; i < 3; i++)
+            for(int j = 0; j < 3; j++){
+                String buttonID = "bt_field" + i + "" + j;
+
+                int resourceID = getResources().getIdentifier(buttonID, "id", getActivity().getPackageName());
+            }
+    }*/
 }
