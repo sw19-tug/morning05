9041ae63f704a948c59263df8f0a7abdc3dede87
stefanie0507 <stefanie.plasch@student.tugraz.at>
2019-03-25 19:03:23 +0100

    GAMES_006 passed first test; added second test: buttons [BL,PS]

--- app/src/androidTest/java/com/example/sw19_morning05/HangmanActivityEspressoTest.java
+++ app/src/androidTest/java/com/example/sw19_morning05/HangmanActivityEspressoTest.java
@@ -32,4 +32,36 @@ public class HangmanActivityEspressoTest {
     {
         onView(withId(R.id.title)).check(matches(isDisplayed()));
     }
+
+    //tests if all keyboard-buttons are displayed
+    @Test
+    public void testIfButtonsExist()
+    {
+        onView(withId(R.id.button_q)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_w)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_e)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_r)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_t)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_z)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_u)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_i)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_o)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_p)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_a)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_s)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_d)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_f)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_g)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_h)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_j)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_k)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_l)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_y)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_x)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_c)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_v)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_b)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_n)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_m)).check(matches(isDisplayed()));
+    }
 }
