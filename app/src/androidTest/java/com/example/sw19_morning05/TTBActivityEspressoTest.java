5cab5888dde84ab7c334ca65cc9b2ab2449a3501
Daniela Graf <daniela.graf@student.tugraz.at>
2019-04-03 10:27:45 +0200

    GAMES_011 Testcase BlockHasNewPosition and basic background tests [DG,JT]

--- app/src/androidTest/java/com/example/sw19_morning05/TTBActivityEspressoTest.java
+++ app/src/androidTest/java/com/example/sw19_morning05/TTBActivityEspressoTest.java
@@ -2,17 +2,20 @@ package com.example.sw19_morning05;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.Button;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertNotEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class TTBActivityEspressoTest
@@ -39,5 +42,37 @@ public class TTBActivityEspressoTest
         onView(withId(R.id.moving_block)).check(matches(withText("TOUCH THE BLOCK")));
 
         //onData(withId(R.id.moving_block)).atPosition()
+        onView(withId(R.id.moving_block)).check(matches(isDisplayed()));
+
+        ttbActivityActivityTestRule.getActivity();
+
+        //View button = ttbActivityActivityTestRule.findViewById(R.id.moving_block);
+
+        Button button = new TTBActivity().findViewById(R.id.moving_block);
+
+        float buttonX = button.getX();
+        float buttonY = button.getY();
+
+        onView(withId(R.id.moving_block)).perform(click());
+
+        float buttonX_new = button.getX();
+        float buttonY_new = button.getY();
+
+        assertNotEquals(buttonX, buttonX_new, 0);
+        assertNotEquals(buttonY, buttonY_new, 0);
+    }
+
+    @Test
+    public void testBackgroundButtonVisible()
+    {
+
+        onView(withId(R.id.background_btn)).check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void testBackgroundIsClickable()
+    {
+        onView(withId(R.id.background_btn)).check(matches(isClickable()));
     }
 }
