54819be9a1fc556b54ce56caf1bea69279142d10
Jasmin Viktoria Dirnboeck <jasmin.dirnboeck@student.tugraz.at>
2019-05-29 09:26:58 +0200

    GAMES_010 add FAB and extend word dialog [DJ, MH]

--- app/src/main/java/com/example/sw19_morning05/ExtendWordsActivity.java
+++ app/src/main/java/com/example/sw19_morning05/ExtendWordsActivity.java
@@ -1,11 +1,19 @@
 package com.example.sw19_morning05;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -13,7 +21,7 @@ import java.util.ArrayList;
 
 public class ExtendWordsActivity extends AppCompatActivity {
 
-    private static HighScoreAdapter highScoreAdapter;
+    private String new_word;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +36,42 @@ public class ExtendWordsActivity extends AppCompatActivity {
                 navigateToWelcomeScreen();
             }
         });
+
+        final FloatingActionButton btn_dialog = findViewById(R.id.btn_hm_extend_words_add);
+        btn_dialog.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                addWordDialog();
+            }
+        });
     }
 
     private void navigateToWelcomeScreen() {
         Intent intent = new Intent(this, HangmanActivity.class);
         startActivity(intent);
+    }
+
+    private void addWordDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add new word");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                new_word = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 
 }
